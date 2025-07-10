7454c7100ab72cebb797a4440c910aeb41a4dd76
HarryYang2369 <y.yang-343@sms.ed.ac.uk>
2025-07-10 17:44:11 +0800

    Ready for deployment to heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -150,7 +150,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     
     # Static properties settings
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
-        
+        os.path.join(BASE_DIR, 'static'),  # noqa: E501
     )
